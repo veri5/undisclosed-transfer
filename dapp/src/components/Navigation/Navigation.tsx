4b695dbbd80d00ac63ef21b9a4ca9bfeae46f9ed
Pablo Buitrago <pablo@briefboards.com>
2024-01-15 15:30:12 -0300

    Added a basic Vite React Dapp.

---
+++ dapp/src/components/Navigation/Navigation.tsx
@@ -0,0 +1,43 @@
+import { useMetaMask } from '~/hooks/useMetaMask'
+import { formatAddress } from '~/utils'
+import styles from './Navigation.module.css'
+
+export const Navigation = () => {
+
+  const { wallet, hasProvider, isConnecting, connectMetaMask, disconnectMetaMask } = useMetaMask()
+
+  return (
+    <div className={styles.navigation}>
+      <div className={styles.flexContainer}>
+        <div className={styles.leftNav}>Undisclosed Transfer</div>
+        <div className={styles.rightNav}>
+          {!hasProvider &&
+            <a href="https://metamask.io" target="_blank" rel="noreferrer">
+              Install MetaMask
+            </a>
+          }
+          {window.ethereum?.isMetaMask && wallet.accounts.length < 1 &&
+            <button disabled={isConnecting} onClick={connectMetaMask}>
+              Connect MetaMask
+            </button>
+          }
+          {hasProvider && wallet.accounts.length > 0 &&
+            <>
+              <a
+                className="text_link tooltip-bottom"
+                href={`https://etherscan.io/address/${wallet}`}
+                target="_blank"
+                data-tooltip="Open in Block Explorer" rel="noreferrer"
+              >
+                {formatAddress(wallet.accounts[0])}
+              </a>
+              <button onClick={disconnectMetaMask}>
+                Disconnect MetaMask
+              </button>
+            </>
+          }
+        </div>
+      </div>
+    </div>
+  )
+}
